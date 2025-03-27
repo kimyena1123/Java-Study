95120bb4490a6e37d17c3e30f96231a8c11e9562
kimyena1123 <kyq104@gmail.com>
2025-03-27 21:38:51 +0900

    iTextAPI를 이용해 책정보를 Pdf 파일에 생성

---
+++ JavaProject0/src/main/java/kr/excel/example/BookInfoToPDF_itext.java
@@ -0,0 +1,60 @@
+package kr.excel.example;
+
+import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.Year;
+import java.util.HashMap;
+
+
+public class BookInfoToPDF_itext {
+    public static void main(String[] args) {
+        HashMap<String, String> bookInfo = new HashMap<>();
+        bookInfo.put("title", "java");
+        bookInfo.put("autor", "kim yena");
+        bookInfo.put("publisher", "korea");
+        bookInfo.put("year", String.valueOf(Year.now().getValue()));
+        bookInfo.put("price", "25000");
+        bookInfo.put("pages", "400");
+
+        try{
+            //PDF 생성을 위한 PdfWriter 객체 생성
+            PdfWriter writer = new PdfWriter(new FileOutputStream("book_infomation.pdf"));
+
+            //PdfWriter 객체를 사용해 PdfDocument 객체 생성
+            PdfDocument pdf = new PdfDocument(writer);
+
+            //Document 객체 생성
+            Document document = new Document(pdf);
+
+            //폰트 생성 및 추가
+//            PdfFont font = PdfFontFactory.createFont("NANUMGOTHICLIGHT.TTF", PdfEncodings.IDENTITY_H, true);
+//            document.setFont(font);
+
+            //책 정보를 문단으로 생성하여 Document에 추가
+            for(String key : bookInfo.keySet()){
+                Paragraph paragraph = new Paragraph(key + ": " + bookInfo.get(key));
+                document.add(paragraph);
+            }
+
+            //Document 닫기
+            document.close();
+
+            System.out.println("book_information.pdf 파일이 생성되었습니다");
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
