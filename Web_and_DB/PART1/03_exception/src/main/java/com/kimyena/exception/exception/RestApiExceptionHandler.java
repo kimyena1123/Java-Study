de446f78001138b70652319d9cef5055389d4226
kimyena1123 <kyq104@gmail.com>
2025-04-01 22:43:34 +0900

    @RestControllerAdvice에서 특정 package나 class 예외처리하기

--- Web_and_DB/PART1/03_exception/src/main/java/com/kimyena/exception/exception/RestApiExceptionHandler.java
+++ Web_and_DB/PART1/03_exception/src/main/java/com/kimyena/exception/exception/RestApiExceptionHandler.java
@@ -1,15 +1,18 @@
 package com.kimyena.exception.exception;
 
+import com.kimyena.exception.controller.RestApiBController;
+import com.kimyena.exception.controller.RestApiController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice //Rest Api가 사용하는 곳에 예외가 일어나는 것을 감지한다.
+//@RestControllerAdvice(basePackages = "com.kimyena.exception.controller") //Rest Api가 사용하는 곳에 예외가 일어나는 것을 감지한다. <- 모든 controller에서 나오는 예외를 잡아준다
+@RestControllerAdvice(basePackageClasses = {RestApiController.class, RestApiBController.class})
 public class RestApiExceptionHandler {
 
-    @ExceptionHandler(value = {Exception.class})
+    @ExceptionHandler(value = {Exception.class}) //모든 예외를 잡음
     public ResponseEntity exception(
             Exception e
     ){
