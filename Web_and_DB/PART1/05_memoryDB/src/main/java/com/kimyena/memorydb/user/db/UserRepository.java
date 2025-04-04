1d82f61ed333809c4f7026dfa10277d1214009fc
kimyena1123 <kyq104@gmail.com>
2025-04-05 00:19:23 +0900

    주석 추가. 현재  DB로 사용X, 메모리로 활용함

--- Web_and_DB/PART1/05_memoryDB/src/main/java/com/kimyena/memorydb/user/db/UserRepository.java
+++ Web_and_DB/PART1/05_memoryDB/src/main/java/com/kimyena/memorydb/user/db/UserRepository.java
@@ -10,6 +10,8 @@ import java.util.List;
 public class UserRepository extends SimpleDataRepository<UserEntity, Long> {
 
     //매개변수에 들어온 점수보다 더 큰 점수를 받은 사람의 정보를 보여주는 메서드
+    //이렇게 원하는 기능을 repository에다가 구현해서 사용하는 방법.
+    //지금같은 경우는 정식 Database(DB)가 없기 때문에, 메모리를 활용했을 때 이런 식으로 활용
     public List<UserEntity> findAllScoreGreaterThen(int score){
 
         return this.findAll().stream() // 전체 list 중에서 stream으로 filter를 걸거다.
