2555d232454af9dda3342ec61a1d65234e20202d
kimyena1123 <kyq104@gmail.com>
2025-04-11 21:29:47 +0900

    intetceptor 알아보기

--- Web_and_DB/PART1/09_Filter/src/main/java/com/kimyena/filter/controller/UserApiController.java
+++ Web_and_DB/PART1/09_Filter/src/main/java/com/kimyena/filter/controller/UserApiController.java
@@ -1,18 +1,18 @@
 package com.kimyena.filter.controller;
 
+import com.kimyena.filter.interceptor.OpenApi;
 import com.kimyena.filter.model.UserRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@OpenApi
 @Slf4j
 @RestController
 @RequestMapping("/api/user")
 public class UserApiController {
 
+    @OpenApi
     @PostMapping("/register") //http://localhost:8080/api/user/register
     public void register(
             @RequestBody
@@ -21,12 +21,15 @@ public class UserApiController {
         log.info("original: {}", userRequest); //original: UserRequest(name=null, phoneNumber=010-1111-1111, email=hone@gmail.com, age=10) <- name보면 정보가 다 안왔다.
     }
 
+    @GetMapping("/hello")
+    public void hello(){
+        log.info("hello");
+    }
+
     @PostMapping("/register1") //http://localhost:8080/api/user/register1
     public void register1( //1, 임시로 디버깅 하는 방법
-            HttpEntity http
+                           HttpEntity http
     ){
         log.info("method 1: {}", http.getBody()); //method 1: {user_name=홍길동, phone_number=010-1111-1111, email=hone@gmail.com, age=10}
     }
-
-
 }
