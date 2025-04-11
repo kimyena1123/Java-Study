dda0e486332fc9420962a736a9a41a968894f9d0
kimyena1123 <kyq104@gmail.com>
2025-04-11 20:12:29 +0900

    filter 알아보기(filterChain)

--- Web_and_DB/PART1/09_Filter/src/main/java/com/kimyena/filter/controller/UserApiController.java
+++ Web_and_DB/PART1/09_Filter/src/main/java/com/kimyena/filter/controller/UserApiController.java
@@ -2,6 +2,7 @@ package com.kimyena.filter.controller;
 
 import com.kimyena.filter.model.UserRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,15 @@ public class UserApiController {
             @RequestBody
             UserRequest userRequest
     ){
-        log.info("{}", userRequest);
+        log.info("original: {}", userRequest); //original: UserRequest(name=null, phoneNumber=010-1111-1111, email=hone@gmail.com, age=10) <- name보면 정보가 다 안왔다.
     }
+
+    @PostMapping("/register1") //http://localhost:8080/api/user/register1
+    public void register1( //1, 임시로 디버깅 하는 방법
+            HttpEntity http
+    ){
+        log.info("method 1: {}", http.getBody()); //method 1: {user_name=홍길동, phone_number=010-1111-1111, email=hone@gmail.com, age=10}
+    }
+
+
 }
