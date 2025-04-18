8ccf436e57847f0aa6922f85850e548835a4f56c
kimyena1123 <kyq104@gmail.com>
2025-04-18 12:40:14 +0900

    (COOKIE 알아보기) cookie 가져오기, 옵션 알아보기

---
+++ Web_and_DB/PART2/02_HTTP_COOKIE/src/main/java/com/kimyena/http_cookie/controller/UserApiController.java
@@ -0,0 +1,61 @@
+package com.kimyena.http_cookie.controller;
+
+import com.kimyena.http_cookie.db.UserRepository;
+import com.kimyena.http_cookie.model.UserDto;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserApiController {
+    //사용자의 정보를 요청했을 때, 해당 유저를 어떻게 식별할 수 있는가
+
+    private final UserRepository _userRepository;
+    private final UserRepository userRepository;
+
+    /**
+     * 방법1. 쿠키를 찾아내는 방법
+     */
+    @GetMapping("/me")
+    public UserDto me(
+            HttpServletRequest httpServletRequest
+    ){
+        //쿠키 받기. 어떤 쿠키가 있는지 확인
+        var cookies = httpServletRequest.getCookies();
+
+        if(cookies != null){
+            for(Cookie cookie : cookies){
+                log.info("key: {}, value : {}", cookie.getName(), cookie.getValue()); //key: authorization-cookie, value : caad7f26-0440-4521-99aa-644213cc966d
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * 방법2. 쿠키를 직접 받는 방법
+     */
+    @GetMapping("/me2")
+    public UserDto me2(
+            @CookieValue(name = "authorization-cookie", required = false)
+            String authorizationCookie
+    ){
+
+        log.info("authorizationCookie: {} ", authorizationCookie);  //authorizationCookie: c52a48f0-f037-4522-9722-746941c95210
+
+        var optionalUserDto = userRepository.findById(authorizationCookie);
+        System.out.println("optionalUserDto로 유저 정보 가져오기: " + optionalUserDto); //Optional[UserDto(id=6e4026b2-2b74-4d1c-b16c-80953864503a, name=홍길동, password=1234)]
+
+        return optionalUserDto.get();
+
+    }
+}
+
--- Web_and_DB/PART2/02_HTTP_COOKIE/src/main/java/com/kimyena/http_cookie/service/UserService.java
+++ Web_and_DB/PART2/02_HTTP_COOKIE/src/main/java/com/kimyena/http_cookie/service/UserService.java
@@ -30,6 +30,9 @@ public class UserService {
                 //cookie에는 domain을 지정해야 한다. cookie는 특정 도메인에서만 활용할 수 있게 되어 있기 때문에
                 cookie.setDomain("localhost"); //naver.com, daum.net, dev.xxx.com <<xxx.com
                 cookie.setPath("/");
+                //쿠키 탈취를 막기 위한
+                cookie.setHttpOnly(true); //자바스크립트에서 해당 값을 읽을 수 없도록 보안처리 해준다.
+                cookie.setSecure(true); // <- https에서만 사용되도록 설정한다. (https에서만 쿠키가 보내지도록 설정)
                 cookie.setMaxAge(-1); //-1이면 연결된 동안에만 사용하겠다. 여기에다가 시간을 지정할 수 있다.
 
                 //cookie 추가
